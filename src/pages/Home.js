b25c04f3d455b74382943a5865a389c58d488f25
Lucas-Github-23 <p.lucas23@escola.pr.gov.br>
2025-03-02 15:27:05 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,28 +5,16 @@ import './Home.css';
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>Welcome!</h1>
+      <h1>Explore Nossos Projetos e Jogos!</h1>
       <p>
-        Este site foi desenvolvido para apresentar uma coleção de projetos e jogos criados pelos
-        alunos do curso técnico em Desenvolvimento de Sistemas do Colégio Santa Maria Goretti.
-        Aqui, você encontrará diversas iniciativas que combinam criatividade, inovação e os conhecimentos
-        adquiridos durante o curso, refletindo o compromisso dos alunos em aplicar a tecnologia para
-        resolver problemas e explorar novas ideias.
+        Este site reúne uma coleção de projetos e jogos desenvolvidos pelos alunos do curso técnico em
+        Desenvolvimento de Sistemas do Colégio Santa Maria Goretti. Aqui, você encontrará iniciativas que
+        combinam criatividade, inovação e tecnologia para resolver problemas e explorar novas ideias.
       </p>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-
+      
       <div className="home-buttons">
-        <Link to="/games" className="home-button">
-          Jogos
-        </Link>
-        <Link to="/projects" className="home-button">
-          Projetos
-        </Link>
+        <Link to="/games" className="home-button">Jogos</Link>
+        <Link to="/projects" className="home-button">Projetos</Link>
       </div>
     </div>
   );
